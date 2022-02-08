40ea2630ead3956139a9c3eb9ccc95cd1f3c3330
msbetsy <78367584+msbetsy@users.noreply.github.com>
2022-02-08 10:35:43 +0100

    Add models: Car, Rate, Manufacturer

--- cars/car_app/models.py
+++ cars/car_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+
+class Car(models.Model):
+    make = models.ForeignKey("Manufacturer", related_name='make', on_delete=models.CASCADE, null=False)
+    model = models.CharField(max_length=250)
+    model_id = models.IntegerField()
+
+    def __str__(self):
+        return "%s %s" % (self.make, self.model)
+
+
+class Manufacturer(models.Model):
+    manufacturer_name = models.CharField(max_length=250, null=False, default=None)
+
+    def __str__(self):
+        return self.manufacturer_name
+
+
+class Rate(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=False)
+    rating = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return "%s %s" % (self.car, self.rating)
--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'car_app',
 ]
 
 MIDDLEWARE = [
