4f4e5b115c44bb3c0b2ecf8dacb7fe9046a69c01
msbetsy <78367584+msbetsy@users.noreply.github.com>
2022-02-09 12:16:02 +0100

    Edit models

--- cars/car_app/models.py
+++ cars/car_app/models.py
@@ -5,24 +5,23 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Car(models.Model):
-    make = models.ForeignKey("Manufacturer", related_name='make', on_delete=models.CASCADE, null=False)
+    make = models.ForeignKey("Manufacturer", related_name='make', on_delete=models.CASCADE)
     model = models.CharField(max_length=250)
-    model_id = models.IntegerField()
 
     def __str__(self):
         return "%s %s" % (self.make, self.model)
 
 
 class Manufacturer(models.Model):
-    manufacturer_name = models.CharField(max_length=250, null=False, default=None)
+    manufacturer_name = models.CharField(max_length=250)
 
     def __str__(self):
         return self.manufacturer_name
 
 
 class Rate(models.Model):
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=False)
-    rating = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     def __str__(self):
         return "%s %s" % (self.car, self.rating)
